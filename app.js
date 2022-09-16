38875931b7d7bac654e2c2dac6f2958fd9eb45dc
shabana kauser <shabanakauser89@gmail.com>
2022-09-16 16:07:34 +0100

    set variable for result and btn as document.querySelector and also set variable for conditions all the possible conditions in an array

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 let cells = ["", "", "", "", "", ""];
 let currentPlayer = "X";
+let result = document.querySelector(".result");
+let btns = document.querySelector(".btn");
+let conditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
